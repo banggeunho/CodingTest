6def1155dd8d69475ec1c298e12f16af21134d0b
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-02-13 16:17:11 +0000

    220213 코딩테스트 - 삼성 sw 아카데미 문제 풀이

--- swexpert/220213.py
+++ swexpert/220213.py
@@ -24,3 +24,85 @@ for tc in range(1, int(input())+1):
     else:
       count += 1
   print(f'#{tc} {count}')
+
+
+# 11588 Calkin-Wilf tree 1
+for tc in range(1, int(input())+1):
+  arr = list(input())
+  a, b = 1, 1
+  for i in arr:
+    if i == 'L':
+      b = a+b
+    else:
+      a = a+b
+  print(f'#{tc} {a} {b}')
+
+# 11445 무한 사전
+for tc in range(1, int(input())+1):
+  p = list(input().rstrip())
+  q = list(input().rstrip())
+  solve = False
+  for i in range(len(p)):
+    if ord(p[i]) < ord(q[i]):
+      solve = True
+      print(f'#{tc} Y')
+      break
+  
+  if not solve:
+    c = 0
+    c1 = 0
+    for i in p:
+      c += ord(i)
+    for i in q:
+      c1 += ord(i)
+    
+    if c1 - c > ord('a'):
+      print(f'#{tc} Y')
+    else:
+      print(f'#{tc} N')
+  
+# 11387 몬스터 사냥
+for tc in range(1, int(input())+1):
+  d, l, n = map(int, input().split())
+  total = d
+  for i in range(2, n+1):
+    total += d*(1+(i-1)*l/100)
+
+  print(f'#{tc} {int(total)}')
+
+# 11315 오목판정
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    matrix = [0] * N
+    for i in range(N):
+        matrix[i] = list(input())
+ 
+    target = ['o'] * 5
+    answer = 'NO'
+ 
+    # 가로 세로 확인 (5개 이상인지 체크)
+    for i in range(N):
+        for j in range(N-4):
+            col = [] # 열방향
+            row = [] # 행방향
+            # 5개 window를 만들어 한칸씩 이동하면서 보겠다.
+            for k in range(5):
+                row.append(matrix[i][j+k])
+                col.append(matrix[j+k][i])
+            if row == target or col == target:
+                answer = 'YES'
+                break
+    # 대각선 확인
+    for i in range(N-4):
+        for j in range(N-4):
+            down = []  # 우하향 대각선
+            up = []  # 우상향 대각선
+            for k in range(5):
+                down.append(matrix[i+k][j+k])
+                up.append(matrix[j+k][N-1-i-k])
+            if down == target or up == target:
+                answer = 'YES'
+                break
+ 
+    print(f'#{tc} {answer}')
