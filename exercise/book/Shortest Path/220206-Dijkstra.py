ab935539e952a3b0f3b130fb5c54ca3cee639f17
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-02-15 08:59:44 +0000

    220215 코딩테스트 준비 - 문제 풀이

--- exercise/book/Shortest Path/220206-Dijkstra.py
+++ exercise/book/Shortest Path/220206-Dijkstra.py
@@ -64,7 +64,7 @@ def dijkstra(start):
       # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
       if cost < distance[i[0]]:
         distance[i[0]] = cost
-        heapq.heappush(cost, i[0])
+        heapq.heappush(q,(cost, i[0]))
 
 # 다익스트라 알고리즘으로
 dijkstra(start)
