b7e5c4ff68db3e1106c87cad90aba315d817ecae
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-02-16 12:05:12 +0000

    220216 코딩테스트 준비 - 카카오 공채 기출문제 풀이

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 코딩테스트 대비 기초 문제 풀이 29일차
-# Date : 2022. 02. 15.
+# Date : 2022. 02. 16.
 # 그리디 문제 풀이
 
 #-------------------------****----------------------------------
@@ -10,38 +10,36 @@
 #-------------------------****----------------------------------
 
 
-# 만들 수 없는 금액
+# 무지의 먹방 라이브 (그리디 문제 -> 우선순위 큐 활용)
+import heapq
 
-# N개의 동전을 이용하여 만들 수 없는 양의 정수 금액 중 최솟값을 구하는 프로그램을 작성해라.
-# 예 N=5, 3, 2, 1, 1, 9원 동전 -> 8원
-# 예 N=3, 3, 5, 7원 동전 -> 1원
+def solution(food_times, k):
+    # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
+    if sum(food_times) <= k:
+        return -1
+    
+    # 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heappush(q, (food_times[i], i+1))
+        
+    sum_values = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    
+    length = len(food_times)
+    
+    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
+    while sum_values + ((q[0][0] - previous) * length) <= k:
+        # 제일 적게 걸리는 음식 시간을 pop 한다.
+        now = heapq.heappop(q)[0]
+        # 꺼내온 음식과 이전에 다 먹은 음식 시간을 빼고 남은 음식 개수를 곱한다.
+        sum_values += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외
+        previous = now # 이전 음식 시간 재설정
+    
+    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+    result = sorted(q, key = lambda x:x[1]) # 음식의 번호 기준으로 정렬하고, 리스트로 만듦
+    return result[(k - sum_values) % length][1] # 전체 시간 중 sum_value를 빼고 남은 음식으로 로테이션 돌린다.
 
-n = int(input())
-coin = list(map(int, input().split()))
-coin.sort()
-
-target = 1
-for i in coin:
-  if target < i:
-    break
-  target += i
-
-print(target)
-
-# 볼링공
-
-n, m = map(int, input().split())
-ball = list(map(int, input().split()))
-count = 0
-
-weight = [0] * 11
-
-for x in ball:
-  weight[x] += 1
-
-for i in range(1, m+1):
-  n -= weight[i] # 무게가 i인 볼링의 개수를 선택하는 경우 제외
-  count += weight[i] * n
-
-print(count)
-
+print(solution([8, 6, 4], 15))
