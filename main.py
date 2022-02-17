db2f8868f30ce2de161c580a286f2df76f8ad492
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-02-17 18:05:54 +0000

    220218 코딩테스트 준비 - 삼성 sw expert academy 문제 풀이

--- main.py
+++ main.py
@@ -9,105 +9,48 @@
 # N의 범위가 10,000,000인 경우 : 시간 복잡도가 O(N)인 알고리즘을 설계
 #-------------------------****----------------------------------
 
-# 9088. 다이아몬드
+# 12222. 문자열 나누기
 
-for i in range(1, int(input())+1):
-  n, k = map(int, input().split())
-  diamond = list()
-  temp = list()
-  max_diamond = 0
-  for j in range(n):
-    diamond.append(int(input()))
+T = int(input())
+for test_case in range(1, T + 1):
+    s = str(input())
+ 
+    # 1. 가장 좋은 부분 문자열의 길이는 1이다
+    # 2. 가능하면 한 글자로 자르고 아니면 한 글자씩 더 붙인다
+    # 3. 인접한 문자열이 중복되지 않도록 검사한다 
+ 
+    count = 0
+    pre = ""
+    idx = 0
+    c = ""
+ 
+    while len(s)>idx:
+        c += s[idx]
+        if c != pre:
+            count += 1
+            pre = c
+            c = ""
+         
+        idx+=1
+ 
+    print(f"#{test_case} {count}")
 
-  diamond.sort()
-  
-  for j in range(0, len(diamond)):
-    temp = []
-    temp.append(diamond[j])
-    for l in range(j+1, len(diamond)):
-      if diamond[l] - diamond[j] <= k:
-        temp.append(diamond[l])
-      
-      else:
-        break
-
-    max_diamond = max(max_diamond, len(temp))
-    
-
-  print(f'#{i} {max_diamond}')
-    
-      
-
-    
-# 7465. 창용 마을 무리의 개수
-# 서로소 집합 문제
-def find_parent(parent, x):
-  # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
-  if parent[x] != x:
-    parent[x] = find_parent(parent, parent[x])
-  return parent[x]
-
-# 두 원소가 속한 집합을 합치기
-def union_parent(parent, a, b):
-  a = find_parent(parent, a)
-  b = find_parent(parent, b)
-
-  # 루트노드의 번호가 더 작은 것으로 바궈줌
-  if a < b:
-    parent[b] = a
-  else:
-    parent[a] = b
+# 10580 전봇대
 
 for tc in range(1, int(input())+1):
-  # 노드와 개수와 간선*(union 연산)의 개수 입력 받기
-  n, m = map(int, input().split())
-  parent = [0] * (n + 1) # 부모 테이블 초기화
-
-  # 부모 테이블상에서, 부모를 자기 자신으로 초기화
-  for i in range(1, n+1):
-    parent[i] = i
-
-  # union 연산을 각각 수행
-  for i in range(m):
-    a, b = map(int, input().split())
-    union_parent(parent, a, b)
-
-  # 각 원소가 속한 집합 출력
-  team = []
-  for i in range(1, n+1):
-    team.append(find_parent(parent, i))
-
-  result = set(team)
-  result = list(result)
-  print(f'#{tc} {len(result)}')
-
-# 12052 부서진 타일
-for tc in range(1, int(input())+1):
-  n, m = map(int, input().split())
-  arr = []
-  solve = True
-
-  for i in range(n):
-    arr.append(list(input()))
+    N = int(input()) # 전선의 개수
+    wires = [list(map(int, input().split())) for i in range(N)]  # 전선 A,B 정보
+    cnt = 0 #교점 개수
+ 
+    # 선 두개씩 조합에서 전봇대 두개에서의 높이차의 부호가 같으면 교점이 없다
+    for i in range(N):
+        for j in range(i+1, N):
+            if (wires[i][0]-wires[j][0])*(wires[i][1]-wires[j][1]) < 0:
+                cnt += 1
+ 
+    print(f'#{tc} {cnt}')
 
 
-  for i in range(n-1):
-    for j in range(m-1):
-      # print(arr[i][j], arr[i][j+1], arr[i+1][j], arr[i+1][j+1])
-      if arr[i][j] == '#' and arr[i][j+1] == '#' and arr[i+1][j] == '#' and arr[i+1][j+1] == '#':
-        arr[i][j] = '.'
-        arr[i][j+1] = '.'
-        arr[i+1][j] = '.'
-        arr[i+1][j+1] = '.'
 
-  for i in range(n):
-    for j in range(m):
-      if arr[i][j] == '#':
-        solve = False
-        break
-  if not solve:
-    print(f'#{tc} NO')
-  else:
-    print(f'#{tc} YES')
 
 
