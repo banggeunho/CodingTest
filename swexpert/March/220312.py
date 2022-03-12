430f002476d2ce71ac28f58ffcb0c6a90c3087c8
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-03-12 09:44:04 +0000

    220312 코딩테스트 준비 - SWEA 문제 풀이

--- swexpert/March/220312.py
+++ swexpert/March/220312.py
@@ -8,6 +8,11 @@
 # N의 범위가 10,000,000인 경우 : 시간 복잡도가 O(N)인 알고리즘을 설계
 #-------------------------****----------------------------------
 
+# 최증증가수열 (Longest Increasing Subsequence)
+# Dynamic Programming
+# case 1 : 순차탐색으로 하나씩 값을 비교해서 보는 법 O(N^2)
+# case 2 : 이진탐색으로 자리의 값을 찾아서 비교해서 보는 법 O(NlogN)
+
 for tc in range(1, int(input())+1):
   n = int(input())
   arr = list(map(int, input().split()))
