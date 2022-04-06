b8d2b7ca207f56233ddf2f6baa36023187ebe6af
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-04-06 06:06:15 +0000

    220405 코딩테스트 준비
    - 백준 문제 풀이

---
+++ 백준/April/1964 신입사원(정렬).py
@@ -0,0 +1,18 @@
+# 1964번 신입 사원
+# 조건에 따라 먼저 서류 순위로 정렬을 한 뒤
+# 남은 면접 순위로 나
+for tc in range(int(input())):
+    n = int(input())
+    arr = []
+    cnt = 1
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+
+    arr.sort()
+    criteria = arr[0][1]
+    for i in range(1, n):
+        if criteria > arr[i][1]:
+            criteria = arr[i][1]
+            cnt += 1
+
+    print(cnt)
