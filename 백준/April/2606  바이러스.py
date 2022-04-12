ba46a1d7a0248048ed7d5605fc01c724c8db058e
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-04-12 06:14:44 +0000

    220412 코딩테스트 준비
    - 백준 문제 풀이

--- 백준/April/2606  바이러스.py
+++ 백준/April/2606  바이러스.py
@@ -1,3 +1,7 @@
+# UNION-FIND(DISJOINT SET), DFS, BFS 세가지 버전으로 문제 풀이
+# 문제 풀이 시간은 셋 다 비슷하나 UNION-FIND < DFS < BFS 순으로 길어짐...
+
+# UNION-FIND
 def find_parent(x):
     global parent
     if parent[x] != x:
@@ -26,4 +30,58 @@ cnt = 0
 for i in range(2, v+1):
     if find_parent(i) == find_parent(1):
         cnt+=1
+print(cnt)
+
+
+
+# DFS
+v = int(input())
+e = int(input())
+graph = [[] for _ in range(v+1)]
+visited = [False]*(v+1)
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+count = -1
+def dfs(start):
+    global count
+    now = start
+    count += 1
+    visited[now] = True
+
+    for i in graph[now]:
+        if not visited[i]:
+            dfs(i)
+
+dfs(1)
+print(count)
+
+
+
+# BFS
+from collections import deque
+v = int(input())
+e = int(input())
+graph = [[] for _ in range(v+1)]
+visited = [False]*(v+1)
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+cnt = -1
+q = deque()
+q.append(1)
+visited[1] = True
+
+while q:
+    now = q.popleft()
+    cnt += 1
+    for i in graph[now]:
+        if not visited[i]:
+            q.append(i)
+            visited[i] = True
+
 print(cnt)
